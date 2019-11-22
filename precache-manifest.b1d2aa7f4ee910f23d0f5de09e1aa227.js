71fa2a31ba1c0829c55ba5d2825e5262c7e25d98
azul-de-prusia <tegthyn_600@hotmail.com>
2019-11-22 17:05:00 -0600

    Updates

--- precache-manifest.b1d2aa7f4ee910f23d0f5de09e1aa227.js
+++ precache-manifest.b1d2aa7f4ee910f23d0f5de09e1aa227.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ee1a90491391f740348aa60747b2c96",
+    "revision": "bee8c49a8359be804b638014963b8dd3",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "70a75df3e1c7aeb5211a",
-    "url": "/portfolio/static/css/main.0b6f50ae.chunk.css"
+    "revision": "f2b3982d75310c6684d0",
+    "url": "/portfolio/static/css/main.256b3cf5.chunk.css"
   },
   {
     "revision": "f8b5439cfd9291608e01",
     "url": "/portfolio/static/js/2.f8db744c.chunk.js"
   },
   {
-    "revision": "70a75df3e1c7aeb5211a",
-    "url": "/portfolio/static/js/main.61f7627c.chunk.js"
+    "revision": "f2b3982d75310c6684d0",
+    "url": "/portfolio/static/js/main.cbe67457.chunk.js"
   },
   {
     "revision": "a46b3fd8f34f8691f45e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0ef7999bd9b0e96526a1d587d74c1378.js"
+  "/portfolio/precache-manifest.b1d2aa7f4ee910f23d0f5de09e1aa227.js"
 );
 
 self.addEventListener('message', (event) => {
