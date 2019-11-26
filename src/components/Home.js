988540b33c8f05ef959b17a2e0c314357107a5bd
azul-de-prusia <tegthyn_600@hotmail.com>
2019-11-25 18:51:31 -0600

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,16 @@ import React, {Component} from 'react';
 class Home extends Component{
     render(){
         return(
-            <div className="About">
-                
+            <div className="home">
+                <p>
+                    Welcome to my Front-End developer jr. portfolio, here you can find some projects i´ve worked on,
+                    by using JavaScript, CSS, HTML, React js, Node js and so on. If you´re interested on my profile,
+                    want to share feedback or leave some comments you can also find the ways to contact me.  
+                </p>
+
+                <p>
+                    This was developed with React js & React router, enjoy.
+                </p>
             </div>
 
         );
