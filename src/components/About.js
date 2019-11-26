d149635cd2cf2b4e5214343e39759838bf8fb69c
azul-de-prusia <tegthyn_600@hotmail.com>
2019-11-25 20:42:47 -0600

    text align

--- src/components/About.js
+++ src/components/About.js
@@ -12,10 +12,11 @@ class About extends Component{
 I have a B.D  in Cultural Heritage Restoration, and through out my 5-year career, I specialized  in paper restoration. 
 I´m also a self taught illustrator and madonnari artist.
 I’m a very creative person who loves to learn new things. Currently I'm starting my career at IT as a Front-End developer and I would love improve my Front-End skills and learn more about UX/UI tools.</p>
-                            <p> I would love to collaborate with a company that appreciates creative work as well as visual and audiovisual content.
-In the long term I would like to develop tools to become a UX/UI designer and create audiovisual content such as animation.</p>
+                            
                     </li>          
                     <li className="small">
+                    <p> I would love to collaborate with a company that appreciates creative work as well as visual and audiovisual content.
+In the long term I would like to develop tools to become a UX/UI designer and create audiovisual content such as animation.</p>
                         <h2>Languages</h2>
                             <p>Native Spanish</p>
                         <p>English B2 level according to the Common European Framework of Reference for Languages (CEFR)</p>
